86846a2a2e73318e010201a8e4fcb49f54da9914
junseo85 <cellojun@hotmail.com>
2026-09-17 02:03:29 -0400

    task 6 done web deployment

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,12 +8,18 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
     emotion = formatted_response['emotionPredictions'][0]['emotion']
+    anger=emotion['anger']
+    disgust=emotion['disgust']
+    fear=emotion['fear']
+    joy=emotion['joy']
+    sadness=emotion['sadness']
+    dominant_emotion=max(emotion, key=emotion.get)
     result = { 
-        'anger': emotion['anger'], 
-        'disgust': emotion['disgust'], 
-        'fear': emotion['fear'], 
-        'joy': emotion['joy'],
-        'sadness': emotion['sadness'],
-        'dominant_emotion': max(emotion, key=emotion.get)
+        'anger': anger, 
+        'disgust': disgust, 
+        'fear': fear, 
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
         }
     return result
