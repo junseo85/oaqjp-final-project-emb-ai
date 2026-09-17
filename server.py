0f23bc3cd7db41cb166dee3c66238e76d23b3d59
junseo85 <cellojun@hotmail.com>
2026-09-17 02:40:53 -0400

    finished the project

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+'''server file
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +7,8 @@ app=Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    '''get data from web and receive data from the server then output
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -13,6 +17,8 @@ def sent_analyzer():
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!."
 
     return (f"For the given statement, the system response is 'anger': {anger}, "
             f"'disgust': {disgust}, "
@@ -22,6 +28,8 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+    ''' render index page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
